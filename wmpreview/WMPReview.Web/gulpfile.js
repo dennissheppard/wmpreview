b26b2f9e216baecfc5d8d7361f4d11b4d4591152
dennissheppard <dennis.sheppard@gmail.com>
2014-12-03 13:40:33 -0600

    gulp stuff

--- wmpreview/WMPReview.Web/gulpfile.js
+++ wmpreview/WMPReview.Web/gulpfile.js
@@ -6,60 +6,66 @@ var plug = require('gulp-load-plugins')();
 var env = plug.util.env;
 var log = plug.util.log;
 var inject = require('gulp-inject');
+var angularFilesort = require('gulp-angular-filesort');
+var webserver = require('gulp-webserver');
+var jshint = require("gulp-jshint");
 
 gulp.task('help', plug.taskListing);
 
 /**
  * @desc Lint the code
  */
-gulp.task('jshint', function () {
-    log('Linting the JavaScript');
-
-    var sources = [].concat(pkg.paths.js);
-    return gulp
-        .src(sources)
-        .pipe(plug.jshint('./.jshintrc'))
-        .pipe(plug.jshint.reporter('jshint-stylish'));
+gulp.task('lint', function () {
+    HintFiles();
 });
 
 /**
  * @desc Watch files and run jshint
  */
-gulp.task('spy', function () {
-    log('Watching JavaScript files');
-
-    var js = ['gulpfile.js'].concat(pkg.paths.js);
-
-    gulp
-        .watch(js, ['jshint'])
-        .on('change', logWatch);
-
-    function logWatch(event) {
-        log('*** File ' + event.path + ' was ' + event.type + ', running tasks...');
-    }
+gulp.task('watch', function() {
+  gulp.watch('src/*.js', ['lint']);
 });
 
-gulp.task('wiredep', function () {
-  gulp.src('./index.html')
-    .pipe(wiredep({
-      directory: './bower_components',
-      ignorePath: './bower_components/'
-    }))
-    .pipe(gulp.dest('.'));
-
-  gulp.src('./index.html')
-    .pipe(wiredep({
-      directory: './bower_components',
-      ignorePath: './'
-    }))
-    .pipe(gulp.dest('.'));
+gulp.task('build', function () {
+  InjectDependecies();
 });
 
-gulp.task('index', function () {
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: true,
+      port: 2323
+    }));
+});
+
+function InjectDependecies(){
   var target = gulp.src('./index.html');
-  // It's not necessary to read the files (will speed up things), we're only after their paths:
-  var sources = gulp.src(['./app/**/*.js', './content/**/*.css'], {read: false});
 
-  return target.pipe(inject(sources))
+  var jsSources = gulp.src('./app/**/*.js').pipe(angularFilesort());
+  var cssSources = gulp.src('./content/**/*.css', {read: false});
+
+//  inject(gulp.src(path.js.compile, {read:false}), {
+//    ignorePath: '/Users/walterroman/Sites/minesweeper-angular',
+//    addRootSlash: false
+//  });
+
+  return target.pipe(inject(jsSources, {relative:true}))
+    .pipe(inject(cssSources, {relative:true}))
+    .pipe(wiredep({
+      directory: './bower_components/',
+      bowerJson: require('./bower.json'),
+      ignorePath: '../..' // bower files will be relative to the root
+    }))
     .pipe(gulp.dest('./'));
-});
+}
+
+function HintFiles(){
+  log('Linting the JavaScript');
+
+  return gulp
+    .src('./app/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+}
